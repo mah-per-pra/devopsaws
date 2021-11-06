10b9ad4c3724b645042f7907780797ad75ac7a17
mahitha <mahitha9902@gmail.com>
2021-11-06 14:35:40 +0530

    second commit

--- Dbutilites.java
+++
@@ -1,8 +0,0 @@
-I am updating for checking
-Hi i am in dev branch
-Hi i am in master branch
-I am updating it in dev branch
-This is a java file
-
-updating again for testing
-
--- Dbutilites.sh
+++
@@ -1,2 +0,0 @@
-This is a java file
-There is an update
